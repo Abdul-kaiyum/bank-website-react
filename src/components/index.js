5d8b91f0218530751b9f17acd7fe6e70c1074115
Abdul-kaiyum <33783648+Abdul-kaiyum@users.noreply.github.com>
2022-11-23 09:58:32 +0600

    this first commit of bank webpage

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Billing from './Billing';
+import Business from './Business';
+import Button from './Button';
+import CardDeal from './CardDeal';
+import Client from './Client';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Hero from './Hero';
+import Navbars from './Navbars';
+import Stats from './Stats';
+import Testimonials from './Testimonials';
+
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Client,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbars,
+    Stats,
+    Testimonials
+
+}
